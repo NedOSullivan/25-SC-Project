3d6500ff84e05e32770678575f3b60dae4e8d318
NedOSullivan <82614972+NedOSullivan@users.noreply.github.com>
2025-12-17 12:01:22 +0000

    add files
    edit README
    add index.html
    add quiz.html
    add otherPage.html
    add images
    add scripts
    add stylesheet

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # 25-SC-Project
+Here you might put a little intro of what this is. in the original GitHub desktop create feature.
+
+-description of website. 
+
+TO DO:
+1. Make contact page
+2. Check contrast of colours
+
+## Sub Heading
+
+### Sub Sub Heading
+
+*italic*
+
+**bold**
+
+~~strikethrough~~
+
+
 
---
+++ script.js
@@ -0,0 +1,43 @@
+// console.log("jeelow,curled")
+
+// function quizOver() { //won't take a parameter
+//     alert("Quiz over. Good work!");
+// }
+
+function checkThem () {
+    //assign "0" to the "score" variable
+    let score = 0;
+    //assign the quiz elements to the quiz variable
+    quiz = document.forms.Quizza.elements;
+    //assign the answer to the question to the answerOne variable
+    answerOne = quiz.italy.value;
+    //check answer to the first question
+    if (answerOne === "espresso") {
+        //add one to the score if the user was right
+        score = score + 1;
+    }
+    answerTwo = quiz.regularly.value;
+    if (answerTwo === "60") {
+        score = score + 1;
+    }
+    answerThree = quiz.temperature.value;
+    if (answerThree === "100DegreesCelsius") {
+        score = score + 1;
+    }
+    answerFour = quiz.rest.value;
+    if (answerFour === "14") {
+        score = score + 1;
+    }
+    answerFive = quiz.producer.value;
+    if (answerFive === "Brasil") {
+        score = score + 1;
+    }
+
+    //add three more questions to HTML and JS. check it regularly.
+
+
+
+    alert ("Nice one, you scored " + score);
+
+
+}
